4734b4fed66026db99f491e2f8f2f4a081bc5890
Daniel Pfeifhofer <Daniel.Pfeifhofer@upstream-mobility.at>
2025-05-17 18:36:09 +0200

    Initial Commit

---
+++ src/main/java/org/example/thesisdemo/ThesisDemoApplication.java
@@ -0,0 +1,12 @@
+package org.example.thesisdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ThesisDemoApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(ThesisDemoApplication.class, args);
+  }
+}
---
+++ src/test/java/org/example/thesisdemo/ThesisDemoApplicationTests.java
@@ -0,0 +1,12 @@
+package org.example.thesisdemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ThesisDemoApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+}
